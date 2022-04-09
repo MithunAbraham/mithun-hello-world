c28d8c5a983d2d8112883e28926e7d38cb8e2cd9
Mithun Abraham <mithun.abraham@edstem.com>
2022-04-09 06:11:57 +0000

    changes

--- lambda_function.py
+++ lambda_function.py
@@ -15,7 +15,13 @@ def wish_hello_world_handler(event, context):
     logging.info(event)
     
     try:
+        temp_file = "/tmp/hello_local.txt"
         s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/hello_local.txt')
+        file = open(temp_file, "r")
+        return {
+        'statusCode': 200,
+        'body': file.read()
+    }
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
          print("The object does not exist.")
